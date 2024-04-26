1fa254cc32e56c4ce17c8feebdeb2fb6a483d55b
Arju <soaatms@gmail.com>
2024-04-26 11:12:41 -0400

    ecommerce project

---
+++ script/index.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+const menuIcon = document.getElementById('menu-icon');
+const menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', () => {
+    if (menu.className === 'hidden') {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+});
